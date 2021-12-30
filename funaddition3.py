3b0980a0e25961b5377dc8159137bd29d4d31532
ajaism <ajeem.pathan@gmail.com>
2021-12-30 12:11:01 +0530

    function example

---
+++ funaddition3.py
@@ -0,0 +1,8 @@
+#funaddition3.py
+def addop(k,v): #function call
+      r = k+v   #process
+      print("sum({},{})={}".format(k,v,r))
+#main program
+m,n=float(input("Enter first value: ")),float(input("Enter Second Value: "))
+addop(m,n)
+
