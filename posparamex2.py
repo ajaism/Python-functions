499b422a25c8e621ef5a8457919af9968f0371db
ajaism <ajeem.pathan@gmail.com>
2021-12-30 20:40:18 +0530

    Positional argument example 2

---
+++ posparamex2.py
@@ -0,0 +1,14 @@
+#posparamex2.py
+def dispstudinfo(sno,sname,marks,crs):
+       print("{}\t{}\t{}\t{}".format(sno,sname,marks,crs))
+#main program
+print("--------------------------------------------------")
+print("Stno\tName\tMarks\tCourse")
+print("---------------------------------------------------")
+dispstudinfo(10,"Shital",66.66,"Python")
+dispstudinfo(20,"Prakash",76.66,"Python")
+dispstudinfo(10,"Shri",86.66,"Python")
+dispstudinfo(10,"Manas",96.66,"Python")
+dispstudinfo(10,"Ajay",56.66,"Python")
+dispstudinfo(10,"Vijay",60.66,"Python")
+print("---------------------------------------------------")
