39616eb4ce1c09d32c51b808eb9a8275bfef227e
ajaism <ajeem.pathan@gmail.com>
2021-12-30 12:03:46 +0530

    example of function

---
+++ funaddiotion2.py
@@ -0,0 +1,13 @@
+#funaddition2.py
+def addop():
+#taking input
+  a = float(input("Enter first value: "))
+  b = float(input("Enter second value: "))
+#process
+  c=a+b
+#display result
+  print("val of a={}".format(a))
+  print("val of b={}".format(b))
+  print("sum ={}".format(c))
+#main program
+addop()
