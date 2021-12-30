744e3717fa8a59ee7a629df37bc644f3f7a5541e
ajaism <ajeem.pathan@gmail.com>
2021-12-30 20:30:53 +0530

    postional Parameter example

---
+++ posparamex1.py
@@ -0,0 +1,14 @@
+#posparamex1.py
+def dispstudinfo(sno,sname,marks):
+     print("{}\t{}\t{}".format(sno,sname,marks))
+# main program
+print("-----------------------------------------")
+print("Stno\tName\tMarks")
+print("------------------------------------------")
+dispstudinfo(10,"Shital",66.66)
+dispstudinfo(20,"Prakash",76.66)
+dispstudinfo(10,"Shri",86.66)
+dispstudinfo(10,"Manas",96.66)
+dispstudinfo(10,"Ajay",56.66)
+dispstudinfo(10,"Vijay",60.66)
+print("-------------------------------------------")
